e898485b938e51b964f7645b254494e634e816a5
sophiadm <sophiadmartin@gmail.com>
2017-10-04 21:45:14 +0100

    refresh

--- planeparts/manager/models.py
+++ planeparts/manager/models.py
@@ -2,14 +2,11 @@ from django.db import models
 
 class PartType(models.Model):
     number = models.CharField(max_length=20)
-    name = models.CharField(max_length=200)
-    itemtype = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
     
     price = models.CharField(max_length=20)
     condition = models.CharField(max_length=50)
     quantity = models.IntegerField(default=1)
-
-    description = models.TextField()   
     def __str__(self):
-        return self.name
+        return self.description
 
